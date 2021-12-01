ac4f048b2b6c193a50f9e44cc63e489d502eb4d8
Harry Lazaridis <harrylaz@kth.se>
2021-12-01 16:17:39 +0100

    sl

--- src/Feed.js
+++ src/Feed.js
@@ -3,8 +3,16 @@ import './App.css';
 
 
 import Header from "./components/Header"
+import Planner from "./service/sl"
 
 export default class Feed extends Component {
+
+    componentDidMount() {
+        Planner(740021730, 740012883)
+            .then( (data) => {
+                console.log(data.Trip);
+            })
+    }
 
     render() {
         return (
