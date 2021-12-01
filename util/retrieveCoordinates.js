395edc3b8f0d53890d5bb2903d264ffc375454df
Maximilian Petersson <maxi.petersson@gmail.com>
2021-12-01 16:19:43 +0100

    Discontinued this file

--- util/retrieveCoordinates.js
+++ util/retrieveCoordinates.js
@@ -1,4 +1,5 @@
 /*
+  --- This file will be discontinued due to uncompatible data in "stops.txt" ---
   Given an origin and a destination, this function returnes the coordinates for each of the stations.
 */
 var fs = require('fs');
@@ -16,12 +17,18 @@ function getCoord(origin, destination){
             lines.forEach(line => {
                 let stationDataArr = line.split(",");
 
-                if (stationDataArr[1] == origin && stationDataArr[6] == "\r") {
+                /*
+                  "" imply that the station is a parent station. Anything else than "" is the id to the parent station.
+                  E.g. Kista Centrum has many substations (children), we want to return the parent.
+                */
+                //console.log(stationDataArr);
+                if (stationDataArr[1] == origin && stationDataArr[5] == "") {
                     ori = stationDataArr;
+                    console.log(ori);
                 }
-
-                if (stationDataArr[1] == destination && stationDataArr[6] == "\r") {
+                if (stationDataArr[1] == destination && stationDataArr[5] == "") {
                     des = stationDataArr;
+                    console.log(des);
                 }
             })
 
