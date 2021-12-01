fa84506645e5f68c4ee55ef482d3f4e8a12da742
Harry Lazaridis <harrylaz@kth.se>
2021-12-01 14:47:47 +0100

    router

---
+++ src/Feed.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './App.css';
+
+
+import Header from "./components/Header"
+
+export default function Feed() {
+
+        return (
+            <div className="feed">
+              <h1>Hello World!</h1>
+            </div>
+          );
+ 
+}
