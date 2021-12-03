ed4660d6dd8b7b7fbd3444dbef4d618b8938b656
Harry Lazaridis <harrylaz@kth.se>
2021-12-03 14:53:36 +0100

    removed test

--- src/App.js
+++ src/App.js
@@ -14,19 +14,6 @@ import { AuthProvider } from './contexts/AuthContext';
 
 import StopFinder from './service/stopFinder.js';
 
-// API call test
-StopFinder({
-  key: "8914fa48-04a5-4511-94c8-61d49831bd6a",
-  input: "Kista",
-  id: "740012883",
-  format: "json"
-})
-  .then( (data) => {
-    console.log(data)
-  }).catch((error)=>{
-    console.log(error)
-  })
-
 export default class App extends Component {
 
   render() {
