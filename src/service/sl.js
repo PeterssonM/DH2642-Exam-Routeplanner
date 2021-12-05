0da3bf7fc14d1ed9b71f773927242067fca3e7e7
Malcolm Liljedahl <malcolm.liljedahl94@gmail.com>
2021-12-05 09:39:50 +0100

    sl.js updated

--- src/service/sl.js
+++ src/service/sl.js
@@ -1,6 +1,6 @@
 let API = "8914fa48-04a5-4511-94c8-61d49831bd6a"
 
-function Planner(originId, destId) {
+export default function Planner(originId, destId) {
     let url = "https://api.resrobot.se/v2/trip?format=json&originId=" + originId + "&destId=" + destId + "&passlist=true&showPassingPoints=true&key=" + API
 
     return new Promise( (resolve, reject) => {
@@ -12,4 +12,3 @@ function Planner(originId, destId) {
     })
 }
 
-export default Planner;
