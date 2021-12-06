d0c2f7a32b6d4e9d83a401b500c30fdaeadeefb6
Malcolm Liljedahl <malcolm.liljedahl94@gmail.com>
2021-12-06 13:18:29 +0100

    Feed.js changed from function component to class component

--- src/Feed.js
+++ src/Feed.js
@@ -3,10 +3,11 @@ import Header from "./components/Header"
 import Planner from "./service/sl"
 import Body from "./components/Body"
 import { nanoid } from '@reduxjs/toolkit';
-import React, {component, useState} from 'react';
+import React, {Component} from 'react';
 
 
-const Feed = () => {
+
+export default class Feed extends Component {
 
     /*componentDidMount() {
         Planner(740021730, 740012883)
@@ -15,47 +16,54 @@ const Feed = () => {
             })
     }*/
 
-    const[notes, setNotes] = useState([{
-            id: nanoid(),
-            title: "Title1",
-            text: "test",   
-            date: "15/12/2020"
-        },
-        {
-            id: nanoid(),
-            title: "Title2",
-            text: "test",
-            date: "11/12/2020"
-        },
-        {
-            id: nanoid(),
-            title: "Title3",
-            text: "test",
-            date: "12/12/2020"
-        },
-        {
-            id: nanoid(),
-            title: "Title4",
-            text: "test",
-            date: "9/12/2020"
-        },
-        {
-            id: nanoid(),
-            title: "Title5",
-            text: "test",
-            date: "4/12/2020"
+    constructor(props) {
+        super(props);
+        this.state = {
+            notes: [{
+                id: nanoid(),
+                title: "Title1",
+                text: "test",   
+                date: "15/12/2020"
+            },
+            {
+                id: nanoid(),
+                title: "Title2",
+                text: "test",
+                date: "11/12/2020"
+            },
+            {
+                id: nanoid(),
+                title: "Title3",
+                text: "test",
+                date: "12/12/2020"
+            },
+            {
+                id: nanoid(),
+                title: "Title4",
+                text: "test",
+                date: "9/12/2020"
+            },
+            {
+                id: nanoid(),
+                title: "Title5",
+                text: "test",
+                date: "4/12/2020"
+            }
+            ]
         }
+    
+    }
+    render() {
+        const {notes} = this.state;
+        return (
+            <div className="feed">
+                <Header/>
+                <Body notes={notes}/>
+            </div>
+        );
+    }
+    
+}
 
-    ]);
-    
-    return (
-        <div className="feed">
-            <Header/>
-            <Body notes={notes}/>
-        </div>
-    );
-    
-};
-export default Feed;
 
 
