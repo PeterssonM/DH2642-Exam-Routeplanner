dba3dad9feb43454402e4f03a96ba583eb348c22
Malcolm Liljedahl <malcolm.liljedahl94@gmail.com>
2021-12-08 16:59:12 +0100

    some files have been changed/mergred

--- src/App.js
+++ src/App.js
@@ -8,22 +8,25 @@ import Header from "./components/presenter/HeaderPresenter"
 import { AuthProvider } from './contexts/AuthContext';
 import StopFinder from './service/stopFinder.js';
 
-export default class App extends Component {
+export default function App() {
 
-  render() {
+  const[darkMode, setDarkMode] = useState(false);
+
     return (
-      <div className="app">
-        <Header />
-        <AuthProvider>
-          <Routes>
-              <Route path='/' element={<Feed />}></Route>
-              <Route path='/home' element={<Feed />}></Route>
-              <Route path="/edit" element={<EditPage />}></Route>
-              <Route path="signup" element={<Signup />}></Route>
-          </Routes>
-        </AuthProvider>
+      <div className={`${darkMode && 'dark-mode'}`}>
+        <div className="app">
+          <Header handleToggleDarkMode={setDarkMode} />
+          <AuthProvider>
+            <Routes>
+                <Route path='/' element={<Feed />}></Route>
+                <Route path='/home' element={<Feed />}></Route>
+                <Route path="/edit" element={<EditPage />}></Route>
+                <Route path="signup" element={<Signup />}></Route>
+            </Routes>
+          </AuthProvider>
+        </div>
       </div>
 
     )
-  }
 }
+
--- src/components/view/HeaderButtonsView.js
+++ src/components/view/HeaderButtonsView.js
@@ -2,15 +2,31 @@ import React from 'react';
 import './HeaderButtons.css';
 import {Link} from 'react-router-dom';
 
-export default function Header( {handleSearchNote} ) {
+export default function Header( {handleSearchNote, handleToggleDarkMode} ) {
 
     return (
             <div className="right-header">
                 <Link to='/home' style={{ textDecoration: 'none' }}>
-               <HeaderButtons icon={"fas fa-home fa-1g"} title="Home"/>
-               </Link>
-               <HeaderButtons icon={"fas fa-sign-out-alt fa-1g"} title="Sign Out"/>
+                    <HeaderButtons icon={"fas fa-home fa-1g"} title="Home"/>
+                </Link>
+                <Link to='/home' style={{ textDecoration: 'none' }}>
+                    <HeaderButtons icon={"fas fa-sign-out-alt fa-1g"} title="Sign Out"/>
+                </Link>
+
+               <div className="darkModeSwitch">
+                    <label className="switch">
+                        <input 
+                            onClick={()=> 
+                                handleToggleDarkMode(
+                                (previousDarkMode)=>!previousDarkMode)
+                            } 
+                        type="checkbox"> 
+                        </input>
+                        <span className="slider round"></span>
+                    </label>
+                </div>
             </div>   
+            
     );
 }
 
