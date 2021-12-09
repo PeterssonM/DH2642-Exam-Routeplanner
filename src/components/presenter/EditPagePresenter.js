094d4abf87207a73b58f70fc6ee59c4055464f66
Malcolm Liljedahl <malcolm.liljedahl94@gmail.com>
2021-12-09 12:35:44 +0100

    EditPageView started

--- src/components/presenter/EditPagePresenter.js
+++ src/components/presenter/EditPagePresenter.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import EditPage from '../view/EditPageView'
 
 function EditPagePresenter() {
     return (
         <div className= "editPage">
+            <EditPage/>
         </div>
     )
 }
--- src/components/view/EditPageView.js
+++ src/components/view/EditPageView.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import "./EditPage.css"
 
 export default function EditPageView() {
     return (
-        <div>
+        <div className="editField">
              <CKEditor
                 editor={ ClassicEditor }
-                data="<p>Hello from CKEditor 5!</p>"
                 onReady={ editor => {
                     // You can store the "editor" and use when it is needed.
                     console.log( 'Editor is ready to use!', editor );
