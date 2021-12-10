c6cda19e9b42e40ceb35442f49e0cfbd1a24fcd0
Harry Lazaridis <harrylaz@kth.se>
2021-12-10 13:49:53 +0100

    can now signup

--- src/presenter/SignupPresenter.js
+++ src/presenter/SignupPresenter.js
@@ -7,13 +7,29 @@ export default function Signup() {
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
 
-    function signup() {
+    const auth = firebase.auth()
+    const db = firebase.firestore()
 
+    async function signup() {
+        try {
+            const res = await auth.createUserWithEmailAndPassword(emailRef.current.value, passwordRef.current.value);
+            const user = res.user;
+            await db.collection("users").add({
+                uid: user.uid,
+                email: emailRef.current.value,
+                authProvider: "local"
+            });
+
+            console.log("Signed up!");
+        } catch (error) {
+            console.log(error);
+            alert(error.message)
+        }
     }
 
     return (
         <div>
-            <form className="signup">
+            <form className="signup" onSubmit={signup}>
                 <div>
                     <input ref={emailRef} type={"text"}/>
                 </div>
