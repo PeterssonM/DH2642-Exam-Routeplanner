8f12b55560effc3139bd56fe4ee05bc21ae58de6
Malcolm Liljedahl <malcolm.liljedahl94@gmail.com>
2021-12-10 14:44:23 +0100

    summaryview changed

--- src/view/SummaryCardView.js
+++ src/view/SummaryCardView.js
@@ -1,29 +1,10 @@
 import React from 'react'
 import "./SummaryCard.css"
 
-export default function SummaryCardView({id, title, text, origin, destination, date}) {
+export default function SummaryCardView() {
     return (
         <div className="card">
-            <div className="summaryCard">
-                {/*<p>{id}</p> debugging*/}
-                <h3 className="summaryCardTitle">TITLE</h3>
-                <div className="summaryCardParagraphBoxLayout">
-                    <div className="summaryCardParagraphBox">
-                        <p className= "summaryCardParagraph">text</p>
-                    </div>
-                </div>
-                <div className="summaryCardSlRouteBoxLayout">
-                    <div className="summaryCardSlRouteBox">
-                        <div className="summaryCardRouteCards">
-                            <p className='originRoute'>Origin: {origin}</p>
-                            <p className='destinationRoute'>Destination: {destination}</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="summaryCardnote-foot">
-                    <small>{date}</small>
-                </div>
-            </div> 
+            
         </div>
     )   
 }
