c64ff87b236421140a22731f924eba6a5cb15f9f
Maximilian Petersson <maxi.petersson@gmail.com>
2021-12-10 18:51:48 +0100

    edited the edit page

--- src/view/EditerView.js
+++ src/view/EditerView.js
@@ -10,69 +10,54 @@ export default function EditPageView() {
     const[noteText, setNoteText] = useState('');
 
     return (
-        <form className="editPage">
-            <div className= "siteBox">
-                <div className="totalBorder">
-                    <div className="editTitleBorder">
-                        <div className="titleBox">
-                            <textarea className="titleArea"
-                                rows={2}
-                                cols={40}
-                                placeholder='Enter a Title..'
-                            ></textarea>
-                        </div>
-                    </div>
-                    <div className="slPlanner">
-                    <div className="border">
-                        <span className="Sltitle">Where do you wanna go?</span>
-                            <div className="layout">
-                                <div className="fromInput">
-                                    <textarea
-                                        className='fromInputTextArea'
-                                        rows="1" 
-                                        cols="40"
-                                        placeholder="From:"
-                                    ></textarea>
-                                </div>
-                                <i className= {"fas fa-exchange-alt fa-1g"} title="exChangeSymbol"/>
-                                <div className="toInput">
-                                    <textarea
-                                        className='toInputTextArea'
-                                        rows="1" 
-                                        cols="40"
-                                        placeholder="To:"
-                                    ></textarea>
-                                </div>
-                            </div>
-                    </div>
+        <form>
+            <div className= "siteContainer">
+                <div className="titleContainer">
+                    <textarea className="titleArea"
+                        rows={2}
+                        cols={40}
+                        placeholder='Enter a Title..'
+                    ></textarea>
                 </div>
-                        <div className="editField">
-                            <CKEditor className="ckEditor"
-                                editor={ ClassicEditor }
-                                onReady={ editor => {
-                                    // You can store the "editor" and use when it is needed.
-                                    console.log( 'Editor is ready to use!', editor );
-                                } }
-                                onChange={ ( event, editor,) => {
-                                    const data = editor.getData();
-                                    console.log( { event, editor, data } );
-                                } }
-                                onBlur={ ( event, editor ) => {
-                                    console.log( 'Blur.', editor );
-                                } }
-                                onFocus={ ( event, editor ) => {
-                                    console.log( 'Focus.', editor );
-                                } }
-                            />
-                        </div>
-                        <div className="editField-Footer">
-                        </div>
-                        <div className="buttonPosition">
-                            <button className="createButton">Create</button>
-                        </div>
-                    </div>                
+                <div className="slStationContainer">
+                    <span className="slTitle">Where do you want to go?</span>
+                    <div className="textBoxLayout">
+                        <textarea className='stationInputTextArea'
+                            rows="1" 
+                            cols="40"
+                            placeholder="From:"
+                        ></textarea>
+                        <div className="arrowsIcon"><i className=  {"fas fa-exchange-alt fa-1g"} title="exChangeSymbol"/></div>
+                        <textarea className='stationInputTextArea'
+                            rows="1" 
+                            cols="40"
+                            placeholder="To:"
+                        ></textarea>
+                    </div>
+                </div>                
+                <div className="ckEditorContainer">
+                    <CKEditor className="ckEditor"
+                        editor={ ClassicEditor }
+                        onReady={ editor => {
+                            // You can store the "editor" and use when it is needed.
+                            console.log( 'Editor is ready to use!', editor );
+                        } }
+                        onChange={ ( event, editor,) => {
+                            const data = editor.getData();
+                            console.log( { event, editor, data } );
+                        } }
+                        onBlur={ ( event, editor ) => {
+                            console.log( 'Blur.', editor );
+                        } }
+                        onFocus={ ( event, editor ) => {
+                            console.log( 'Focus.', editor );
+                        } }
+                    />
+                </div>
+                <div className="buttonPosition">
+                    <button className="createButton">Create</button>
+                </div>
             </div>
         </form>
-        
     )
 }
