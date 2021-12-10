960e3d269dcbcc39d86f657bc6de4edd9813d5fb
Malcolm Liljedahl <malcolm.liljedahl94@gmail.com>
2021-12-10 14:41:34 +0100

    signup page done

--- src/view/SignUpView.js
+++ src/view/SignUpView.js
@@ -5,18 +5,25 @@ export default function SignUpView( {signup, emailRef, passwordRef, passwordConf
     return (
             <div className="signUpBox">
                 <form className="signup" onSubmit={signup}>
-                    <div>
-                        <input ref={emailRef} type={"text"}/>
+                    <div className="signUpTitleLocation">
+                        <span className="signUpTitle">Sign Up</span>
                     </div>
-                    <div>
-                        <input ref={passwordRef} type={"password"} />
-                    </div>
-                    <div>
-                        <input ref={passwordConfirmRef} type={"password"} />
-                    </div>
-                    <div>
-                        <button type="submit">Signup</button>
-                    </div>
+                    <label className="email">
+                        <input 
+                        ref={emailRef} type={"text"}
+                        placeholder='Email'/>
+                    </label>
+                    <label className="password">
+                        <input ref={passwordRef} type={"password"}
+                        placeholder='Password' />
+                    </label>
+                    <label className="confirmPassword">
+                        <input ref={passwordConfirmRef} type={"password"}
+                        placeholder='Confirm Password' />
+                    </label>
+                    <label>
+                        <button className='signUpButton' type="submit">Signup</button>
+                    </label>
                 </form>
             </div>
     )
