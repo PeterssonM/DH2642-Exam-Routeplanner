382d03b3537f341b347f4f75b7c08fb743f0cdef
Malcolm Liljedahl <malcolm.liljedahl94@gmail.com>
2021-12-16 14:13:33 +0100

    updates

--- src/view/SummaryCardView.js
+++ src/view/SummaryCardView.js
@@ -10,34 +10,32 @@ export default function SummaryCardView({data, title, body}) {
             <div className="titleContainer">
                 <h2>{title}</h2>
             </div>
-
             <div className="textContainer" dangerouslySetInnerHTML={{__html: body}}>
             </div>
-
             <div className="routePlannerContainer">
-                <table>
-                    <thead>
+                <table className="routeTableContainer">
+                    <thead className="travelTitle">
                         <h3>DIN RESA!</h3>
                     </thead>
-                    <tbody>
-                        {data && data.stops.map(stop => {
-                            console.log(stop);
-                            return(
-                                <div>
-                                    <tr key={stop.direction}><h3>Mot: {stop.direction}</h3></tr>
+                    <div className="travelData">
+                        <tbody>
+                            {data && data.stops.map(stop => {
+                                console.log(stop);
+                                return(
+                                    <div>
+                                        <tr key={stop.direction}><h3>Mot: {stop.direction}</h3></tr>
 
-                                    {stop.stops.Stop && stop.stops.Stop.map(station => {
-                                        return( <tr key={station.id}> {station.name}</tr> )
-                                    })}
-                                </div>
-                            )
-                        })}
-                    </tbody>
+                                        {stop.stops.Stop && stop.stops.Stop.map(station => {
+                                            return( <tr key={station.id}> {station.name}</tr> )
+                                        })}
+                                    </div>
+                                )
+                            })}
+                        </tbody>
+                    </div>
                 </table>
 
             </div>
-
-          
         </div>
     )
 
