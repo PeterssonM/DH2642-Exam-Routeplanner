26e3b314237342509e6b9a843bf85598243b1998
Maximilian Petersson <maxi.petersson@gmail.com>
2021-12-17 16:09:23 +0100

    new layout on small displays, css optimisation

--- src/view/HeaderView.js
+++ src/view/HeaderView.js
@@ -24,28 +24,61 @@ export default function Header({signout, showSearchBar, filterNotes, email}) {
     }
 
     return (
-        <div className="headerContainer">
-            <div className="left-headerContainer">
-                {showSearchBar &&
-                    <div className="header_search">
-                        <div className="searchIcon">
-                            <i className= {"fas fa-search fa-1g"} title="Search"/>
-                        </div>
-                        <input className="inputBar" onChange={(e) => filterNotes(e.currentTarget.value)} type="text" placeholder="Type to search..."></input>
+        <div>
+            <div className="big">
+                <div className="headerContainer">
+                    <div className="left-headerContainer">
+                        {showSearchBar &&
+                            <div className="header_search">
+                                <div className="searchIcon">
+                                    <i className= {"fas fa-search fa-1g"} title="Search"/>
+                                </div>
+                                <input className="inputBar" onChange={(e) => filterNotes(e.currentTarget.value)} type="text" placeholder="Type to search..."></input>
+                            </div>
+                        }
                     </div>
-                }
+                    <div className="middle-headerContainer">
+                        <span className="headerTitle">Tenta Schedueler</span>
+                    </div>
+                    <div className="right-headerContainer">
+                        <Link to='/home' style={{ textDecoration: 'none' }}>
+                            <HeaderButtons icon={"fas fa-home fa-1g"} title="Home"/>
+                        </Link>
+                        <AccountIcon className="accountSymbol" icon={"fas fa-user fa-1g"} title={email}/>
+                        <Link to='/signin' style={{ textDecoration: 'none' }}>
+                            <HeaderButtons className="homeButton" icon={"fas fa-sign-out-alt fa-1g"} title="Sign Out" onClick={signout}/>
+                        </Link>
+                    </div>
+                </div>
             </div>
-            <div className="middle-headerContainer">
-                <span className="headerTitle">Tenta Schedueler</span>
-            </div>
-            <div className="right-headerContainer">
-                    <Link to='/home' style={{ textDecoration: 'none' }}>
-                        <HeaderButtons icon={"fas fa-home fa-1g"} title="Home"/>
-                    </Link>
-                    <AccountIcon className="accountSymbol" icon={"fas fa-user fa-1g"} title={email}/>
-                    <Link to='/signin' style={{ textDecoration: 'none' }}>
-                        <HeaderButtons className="homeButton" icon={"fas fa-sign-out-alt fa-1g"} title="Sign Out" onClick={signout}/>
-                    </Link>
+
+            <div className="small">
+                <div className="headerSmallContianer">
+                    <div className="headerUpperContainer">
+                        <div className="left_headerContainer">
+                            <span className="headerTitle">Tenta Schedueler</span>
+                        </div>
+                        <div className="right_headerContainer">
+                            <Link to='/home' style={{ textDecoration: 'none' }}>
+                                <HeaderButtons icon={"fas fa-home fa-1g"} title="Home"/>
+                            </Link>
+                            <AccountIcon className="accountSymbol" icon={"fas fa-user fa-1g"} title={email}/>
+                            <Link to='/signin' style={{ textDecoration: 'none' }}>
+                                <HeaderButtons className="homeButton" icon={"fas fa-sign-out-alt fa-1g"} title="Sign Out" onClick={signout}/>
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="headerLowerContainer">
+                        {showSearchBar &&
+                            <div className="header_small_search">
+                                <div className="searchSmallIcon">
+                                    <i className= {"fas fa-search fa-1g"} title="Search"/>
+                                </div>
+                                <input className="inputSmallBar" onChange={(e) => filterNotes(e.currentTarget.value)} type="text" placeholder="Type to search..."></input>
+                            </div>
+                        }
+                    </div>
+                </div>
             </div>
         </div>
     );
